352172489c02e5ed1a1aaafba4b8616ed5395d4c
Chaiwat Suwannarat <66022523@up.ac.th>
2024-11-22 23:25:42 +0700

    🔢 Add age calculator function.

--- source/main.js
+++ source/main.js
@@ -96,3 +96,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// ? Calculate age
+const aboutAge = document.getElementById("about-age");
+const birthYear = 2005;
+const currentYear = new Date().getFullYear();
+const currentAge = currentYear - birthYear;
+
+aboutAge.textContent = currentAge;

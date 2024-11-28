e95ec752ba3329b4242b36f552d672edc99389fe
Chaiwat Suwannarat <66022523@up.ac.th>
2024-11-28 16:54:57 +0700

    🪢 Create main scripts for integrate to once file.

---
+++ source/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  import("./calculatorAge.js");
+  import("./scrollSpy.js");
+});
+
+import("./carouselAutoplay.js")
+import("./themeController.js");

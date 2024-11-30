23c070986aa76ac0340916e4083baccd4a03517f
Chaiwat Suwannarat <66022523@up.ac.th>
2024-11-30 20:17:53 +0700

    🪢 Integrate in main.js file.

--- source/scripts/calculateAge.js
+++
@@ -1,6 +0,0 @@
-const aboutAge = document.getElementById("about-age");
-const birthYear = 2005;
-const currentYear = new Date().getFullYear();
-const currentAge = currentYear - birthYear;
-
-aboutAge.textContent = currentAge;

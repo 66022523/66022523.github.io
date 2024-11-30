fd3e5f38a52c4750d4c59e3d75b3844c235deb50
Chaiwat Suwannarat <66022523@up.ac.th>
2024-11-30 20:21:04 +0700

    🔧 Reduce code lines.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  import("./calculateAge.js");
+  import("./carouselAutoplay.js");
   import("./scrollSpy.js");
-});
+  import("./themeController.js");
 
-import("./carouselAutoplay.js");
-import("./themeController.js");
+  // Set current age in about section
+  const aboutAgeElement = document.getElementById("about-age");
+  aboutAgeElement.textContent = new Date().getFullYear() - 2005;
+
+  // Set current year in footer
+  const currentYearElement = document.getElementById("current-year");
+  currentYearElement.textContent = new Date().getFullYear();
+});

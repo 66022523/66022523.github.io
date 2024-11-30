a3851067dd1d9dcb5a33767a3b9336d1176b5132
Chaiwat Suwannarat <66022523@up.ac.th>
2024-11-30 23:47:33 +0700

    🔍 Update source config.

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default [
   pluginJs.configs.recommended,
   {
     ...html.configs["flat/recommended"],
-    files: ["/source/**/*.html"],
+    files: ["**/*.html"],
     plugins: {
       "@html-eslint": html,
     },

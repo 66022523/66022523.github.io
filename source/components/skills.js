355edefbfd7e116a2e4c33dfa2df5c61d6939384
Chaiwat Suwannarat <66022523@up.ac.th>
2024-12-03 01:37:33 +0700

    📏 Reduce characters length and optimize.

--- source/components/skills.js
+++ source/components/skills.js
@@ -2,78 +2,67 @@ import configs from "../configs/index.js";
 import { getAverageColor } from "../scripts/utils.js";
 
 const fragment = document.createDocumentFragment();
-const skillCarouselItemsElement = document.getElementById(
-  "skill-carousel-items",
-);
-const skillCarouselIndicatorsElement = document.getElementById(
-  "skill-carousel-indicators",
-);
+const skillItemsElement = document.getElementById("skill-items");
+const skillIndicatorsElement = document.getElementById("skill-indicators");
+
+const maxItems = 8;
 
 configs.skills.forEach((skill, index) => {
-  if (index % 8 === 0) {
+  if (index % maxItems === 0) {
     const carouselItem = document.createElement("div");
     carouselItem.className = "carousel-item grid w-full gap-4 lg:grid-cols-4";
-    carouselItem.id = `skill-item-${index / 8}`;
+    carouselItem.id = `skill-item-${index / maxItems}`;
     fragment.appendChild(carouselItem);
 
-    // Create indicator
     const indicator = document.createElement("a");
-    indicator.href = `#skill-item-${index / 8}`;
+    indicator.href = `#skill-item-${index / maxItems}`;
     indicator.className = "btn btn-error btn-soft btn-xs";
-    indicator.textContent = index / 8 + 1;
-    skillCarouselIndicatorsElement.appendChild(indicator);
+    indicator.textContent = index / maxItems + 1;
+    skillIndicatorsElement.appendChild(indicator);
   }
 
-  // Create card
   const card = document.createElement("div");
   card.className =
     "card overflow-clip backdrop-blur before:absolute before:inset-0 before:-z-10 before:bg-cover before:bg-center before:blur-2xl";
 
   const figure = document.createElement("figure");
-  figure.className = "px-10 pt-10";
-
-  // Create and set up image
-  const img = document.createElement("img");
-  // img.loading = "lazy";
-  img.src = skill["source"];
-  img.alt = skill["alternate"];
-  img.width = 48;
-  img.height = 48;
-  img.className = "size-12 rounded";
-
-  // Get average color when image loads
-  img.onload = () => {
-    const { r, g, b } = getAverageColor(img);
+  const image = document.createElement("img");
+  image.loading = "lazy";
+  image.src = skill.source ?? "";
+  image.alt = skill.alternate ?? "";
+  image.width = 48;
+  image.height = 48;
+  image.className = "size-12 rounded";
+  image.onload = () => {
+    const { r, g, b } = getAverageColor(image);
     card.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.5)`;
   };
+  figure.className = "px-10 pt-10";
+  figure.appendChild(image);
 
-  // Create card body
   const body = document.createElement("div");
   body.className = "card-body items-center text-center";
 
-  // Create skill title
-  const title = document.createElement("h2");
-  title.textContent = skill["title"] ?? "";
-  title.className = "card-title";
+  if (skill.title) {
+    const title = document.createElement("h2");
+    title.textContent = skill.title ?? "";
+    title.className = "card-title";
+    body.appendChild(title);
+  }
+  if (skill.type) {
+    const type = document.createElement("div");
+    type.textContent = skill.type ?? "";
+    type.className = "badge badge-neutral";
+    body.appendChild(type);
+  }
+  if (skill.description) {
+    const description = document.createElement("p");
+    description.textContent = skill.description ?? "";
+    body.appendChild(description);
+  }
 
-  // Create skill types
-  const type = document.createElement("div");
-  type.textContent = skill["type"] ?? "";
-  type.className = "badge badge-neutral";
-
-  // Create skill description
-  const description = document.createElement("p");
-  description.textContent = skill["description"] ?? "";
-
-  // Attach the created elements
-  figure.appendChild(img);
-  card.appendChild(figure);
-  title && body.appendChild(title);
-  type && body.appendChild(type);
-  description && body.appendChild(description);
-  card.appendChild(body);
-
+  card.append(figure, body);
   fragment.lastChild.appendChild(card);
 });
 
-skillCarouselItemsElement.appendChild(fragment);
+skillItemsElement.appendChild(fragment);
